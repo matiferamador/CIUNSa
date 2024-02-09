b143108f0d59ce9e45ac7ae448150109bef5a533
matiferamador <107516034+matiferamador@users.noreply.github.com>
2024-02-09 10:49:01 -0300

    se agrego pagina author

--- wp-content/themes/ciunsathemes/template-parts/blog.php
+++ wp-content/themes/ciunsathemes/template-parts/blog.php
@@ -9,7 +9,7 @@
             </a>
             <p class="meta">
                 <span>Por: </span>
-                <a href=""> 
+                <a href="<?php echo get_author_posts_url( get_the_author_meta('ID')); ?>"> 
                     <?php echo get_the_author_meta('display_name'); ?>
                 </a>
             </p>
