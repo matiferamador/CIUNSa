2ce768f6ca727e495a9932d6b116218b94b37520
matiferamador <107516034+matiferamador@users.noreply.github.com>
2024-02-09 11:19:56 -0300

    guardar cambios

--- wp-content/themes/ciunsathemes/template-parts/post.php
+++ wp-content/themes/ciunsathemes/template-parts/post.php
@@ -8,8 +8,12 @@
                 <div class="meta-info">
                         <p class="meta">
                         <span>Por: </span>
+                        <a href="<?php echo get_author_posts_url( get_the_author_meta('ID')); ?>">
+                        <?php echo get_the_author_meta('display_name'); ?>
+
+
+                        </a>
                      
-                            <?php echo get_the_author_meta('display_name'); ?>
                         
                         </p>
                         
